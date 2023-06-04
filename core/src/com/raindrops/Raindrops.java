bf9d757157d5de7cd08cac19bb7187f4edb72cd6
sedar <desiradjysedar@gmail.com>
2023-06-04 14:53:24 +0200

    Initial commit

---
+++ core/src/com/raindrops/Raindrops.java
@@ -0,0 +1,65 @@
+package com.raindrops;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class Raindrops extends Game {
+    private Texture dropImage;
+    private Texture bucketImage;
+    private Sound dropSound;
+    private Music rainMusic;
+    private OrthographicCamera camera;
+    public SpriteBatch batch;
+    private Rectangle bucket;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+    public BitmapFont font;
+
+
+
+    @Override
+    public void create() {
+
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // use libGDX's default Arial font
+        this.setScreen(new MainMenuScreen(this));
+
+
+
+
+
+        // ... more to come ...
+    }
+
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800-64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+
+    public void render () {
+        super.render(); // important!
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+
+    }
+
+}
